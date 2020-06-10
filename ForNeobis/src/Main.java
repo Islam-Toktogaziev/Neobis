5aa0f491a318ae6ad3d43805d948f7f6e4a67313
Islam <Islam010201@gamil.com>
2020-06-10 13:10:49 +0600

    Part 2

---
+++ ForNeobis/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void Hello_world(){
+        for ( int i=0;i<10;i-=-1){
+            System.out.println ("Hello world");
+        }
+    }
+
+    public static void main(String[] args) {
+        Hello_world();
+    }
+
+}
